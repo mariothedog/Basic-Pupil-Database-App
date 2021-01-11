7fa72ae1a1b7c2c9a13531a1f775f072994f0799
mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-01-11 17:21:16 +0000

    Refactor FrameMainMenu widget creation for readibility

--- src/frame_main_menu.py
+++ src/frame_main_menu.py
@@ -10,15 +10,19 @@ class FrameMainMenu(FramePage):
         self.rowconfigure(1, weight=1)
         self.columnconfigure(0, weight=1)
 
-        tk.Label(self, text="Pupil Database", font=(
-            "TkDefaultFont", 16)).grid(sticky=tk.S)
+        label_header = tk.Label(
+            self, text="Pupil Database", font=("TkDefaultFont", 16))
+        label_header.grid(sticky=tk.S)
 
         frame_entry = tk.Frame(self, relief=tk.RIDGE, borderwidth=1)
         frame_entry.grid(row=1, column=0, pady=(10, 0), sticky=tk.N)
 
-        tk.Button(frame_entry, text="Login", width=6, command=lambda: self.app.show_frame(
-            frame_login.FrameLogin)).grid(padx=5, pady=5)
-        tk.Button(frame_entry, text="Register", width=6, command=lambda: self.app.show_frame(
-            frame_register.FrameRegister)).grid(row=0, column=1, padx=5, pady=5)
-        tk.Button(frame_entry, text="Exit", width=6, command=self.app.parent.destroy).grid(
-            row=1, column=0, columnspan=2, pady=(0, 5))
+        button_login = tk.Button(frame_entry, text="Login", width=6,
+                                 command=lambda: self.app.show_frame(frame_login.FrameLogin))
+        button_register = tk.Button(frame_entry, text="Register", width=6,
+                                    command=lambda: self.app.show_frame(frame_register.FrameRegister))
+        button_exit = tk.Button(frame_entry, text="Exit",
+                                width=6, command=self.app.parent.destroy)
+        button_login.grid(padx=5, pady=5)
+        button_register.grid(row=0, column=1, padx=5, pady=5)
+        button_exit.grid(row=1, column=0, columnspan=2, pady=(0, 5))
