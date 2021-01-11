8fa2f50ca9ccde826923807c7726e1de304b60b7
mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-01-11 17:25:05 +0000

    Refactor FrameAddPupil widget creation for readibility

--- src/menu_options/frame_add_pupil.py
+++ src/menu_options/frame_add_pupil.py
@@ -17,20 +17,24 @@ class FrameAddPupil(FramePage):
         frame_entry = tk.Frame(self, relief=tk.RIDGE, borderwidth=1)
         frame_entry.grid(columnspan=3, pady=10)
 
-        tk.Label(frame_entry, text="Pupil Name").grid()
+        label_name = tk.Label(frame_entry, text="Pupil Name")
         self.entry_pupil_name = tk.Entry(frame_entry, text="Pupil Name")
+        label_name.grid()
         self.entry_pupil_name.grid(row=0, column=1)
 
         frame_buttons = tk.Frame(self)
         frame_buttons.grid(columnspan=3, pady=(0, 20))
 
-        tk.Button(frame_buttons, text="Cancel", command=lambda: self.app.show_frame(
-            frame_option_menu.FrameOptionMenu)).grid(padx=(0, 5), sticky=tk.E)
-        tk.Button(frame_buttons, text="Add Pupil", command=self.add_pupil).grid(
-            row=0, column=2, padx=(5, 0), sticky=tk.W)
+        button_cancel = tk.Button(frame_buttons, text="Cancel", command=lambda: self.app.show_frame(
+            frame_option_menu.FrameOptionMenu))
+        button_add = tk.Button(
+            frame_buttons, text="Add Pupil", command=self.add_pupil)
+        button_cancel.grid(padx=(0, 5), sticky=tk.E)
+        button_add.grid(row=0, column=2, padx=(5, 0), sticky=tk.W)
 
-        tk.Label(self, text="Pupils", font=("TkDefaultFont", 14)).grid(
-            row=2, column=1, sticky=tk.S)
+        label_header_pupils = tk.Label(
+            self, text="Pupils", font=("TkDefaultFont", 14))
+        label_header_pupils.grid(row=2, column=1, sticky=tk.S)
 
         self.frame_pupils = ScrollingListbox(self)
         self.frame_pupils.grid(row=3, column=1, sticky=tk.N)
@@ -55,7 +59,7 @@ class FrameAddPupil(FramePage):
         self.entry_pupil_name.delete(0, tk.END)
 
         self.frame_pupils.list_variable.set(pupils)
-    
+
     def on_show(self):
         with open(constants.JSON_ACCOUNTS, "a+") as file:
             file.seek(0)
