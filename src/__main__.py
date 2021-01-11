92a97aacb3ea363b23bf7615b775c8b591ae3ed5
mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-01-11 23:13:57 +0000

    Remove black border

--- src/__main__.py
+++ src/__main__.py
@@ -5,6 +5,5 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.geometry("320x320")
     root.title("Pupil Database")
-    app = Application(parent=root, highlightbackground="black",
-                      highlightthickness=3)
+    app = Application(parent=root)
     app.mainloop()
