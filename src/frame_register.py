92ae7b487eda6440739b41672bf0ed40b9b23531
mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-01-11 17:23:46 +0000

    Refactor FrameRegister widget creation for readibility

--- src/frame_register.py
+++ src/frame_register.py
@@ -17,22 +17,26 @@ class FrameRegister(FramePage):
         frame_entry = tk.Frame(self, relief=tk.RIDGE, borderwidth=1)
         frame_entry.grid(columnspan=2, pady=(0, 5), sticky=tk.S)
 
-        tk.Label(frame_entry, text="Username").grid()
+        label_username = tk.Label(frame_entry, text="Username")
+        label_password = tk.Label(frame_entry, text="Password")
+        label_password_confirm = tk.Label(frame_entry, text="Confirm Password")
+        label_username.grid()
+        label_password.grid()
+        label_password_confirm.grid()
+
         self.entry_username = tk.Entry(frame_entry, width=15)
-        self.entry_username.grid(row=0, column=1)
-
-        tk.Label(frame_entry, text="Password").grid()
         self.entry_password = tk.Entry(frame_entry, width=15, show="*")
-        self.entry_password.grid(row=1, column=1)
-
-        tk.Label(frame_entry, text="Confirm Password").grid()
         self.entry_password_confirm = tk.Entry(frame_entry, width=15, show="*")
+        self.entry_username.grid(row=0, column=1)
+        self.entry_password.grid(row=1, column=1)
         self.entry_password_confirm.grid(row=2, column=1)
 
-        tk.Button(self, text="Cancel", width=5, command=lambda: self.app.show_frame(
-            frame_main_menu.FrameMainMenu)).grid(row=1, column=0, padx=(0, 15), sticky=tk.N+tk.E)
-        tk.Button(self, text="Register", width=5, command=self.register).grid(
-            row=1, column=1, padx=(15, 0), sticky=tk.N+tk.W)
+        button_cancel = tk.Button(self, text="Cancel", width=5, command=lambda: self.app.show_frame(
+            frame_main_menu.FrameMainMenu))
+        button_register = tk.Button(
+            self, text="Register", width=5, command=self.register)
+        button_cancel.grid(row=1, column=0, padx=(0, 15), sticky=tk.N+tk.E)
+        button_register.grid(row=1, column=1, padx=(15, 0), sticky=tk.N+tk.W)
 
     def register(self):
         username = self.entry_username.get()
