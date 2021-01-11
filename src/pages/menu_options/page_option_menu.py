c9be3fd57da88601ef32b342cdd91f949063fe2c
mariothedog <28541089+mariothedog@users.noreply.github.com>
2021-01-11 18:28:39 +0000

    Add functional amend_pupil menu

--- src/pages/menu_options/page_option_menu.py
+++ src/pages/menu_options/page_option_menu.py
@@ -2,6 +2,7 @@ import tkinter as tk
 
 from pages.frame_page import FramePage
 from pages.menu_options.page_add_pupil import PageAddPupil
+from pages.menu_options.page_amend_pupil import PageAmendPupil
 
 
 class PageOptionMenu(FramePage):
@@ -13,7 +14,8 @@ class PageOptionMenu(FramePage):
         BUTTON_WIDTH = 10
         tk.Button(self, text="Add Pupil", width=BUTTON_WIDTH,
                   command=lambda: self.app.show_page(PageAddPupil)).grid(sticky=tk.S)
-        tk.Button(self, text="Amend Pupil", width=BUTTON_WIDTH).grid()
+        tk.Button(self, text="Amend Pupil", width=BUTTON_WIDTH,
+                  command=lambda: self.app.show_page(PageAmendPupil)).grid()
         tk.Button(self, text="Search", width=BUTTON_WIDTH).grid()
         tk.Button(self, text="Quiz", width=BUTTON_WIDTH).grid()
         tk.Button(self, text="Log Out", width=BUTTON_WIDTH,
